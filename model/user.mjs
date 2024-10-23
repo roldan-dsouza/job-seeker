1c8dc278f10a42b6ca0b8d3759d4110d016695f8
Roldan <roldannov@gmail.com>
2024-10-23 13:37:20 +0530

    userSchema

---
+++ model/user.mjs
@@ -0,0 +1,23 @@
+import mongoose, { Schema } from "mongoose";
+
+const userschema = Schema({
+  userName: { type: String, required: true },
+  pdfAddress: { type: URL, unique: true, required: true },
+  location: { type: String, required: true },
+  jobTitle: [{ type: String, required: true }],
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    validate: {
+      validator: function (v) {
+        return /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/.test(v);
+      },
+      message: (props) => `${props.value} is not a valid email!`,
+    },
+  },
+  password: { type: String, required: true },
+  appliedJobs: { type: String, required: false },
+});
+
+const user = mongoose.model("user", userschema);
