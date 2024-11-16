601967988aee34f3305e69ac1e9ba709106d07ed
Roldan <roldannov@gmail.com>
2024-11-16 16:22:37 +0530

    minor fixes with cors

--- server.mjs
+++ server.mjs
@@ -24,13 +24,16 @@ mongoose
   });
 
 //app.use(express.urlencoded({ extended: true }));
-app.use(
-  cors({
-    origin: "http://localhost:3000",
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    allowedHeaders: ["Content-Type", "Authorization"],
-  })
-);
+const corsOptions = {
+  origin: function (origin, callback) {
+    callback(null, true); // Allow any origin (passing null allows dynamic origin)
+  },
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+  credentials: true, // Allow credentials (cookies, authorization headers)
+};
+
+app.use(cors(corsOptions));
 app.use(helmet());
 app.use(express.json());
 app.use(cookieParser());
